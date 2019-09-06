233eac1cfccce818b9b1f7750ef8a325bca3fa4d
Francois Du Plessis <Francois.DuPlessis@Euromonitor.com>
2019-09-06 10:28:31 +0200

    Initial commit for floating point value

---
+++ FloatingPointLibrary/FloatingPointLibrary.Unit.Test/FloatingPointValueTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace FloatingPointLibrary.Unit.Test
+{
+    public class FloatingPointValueTest
+    {
+        [Fact]
+        public void Parse_ValidString_ReturnsKataFloat32RepresentationOfString()
+        {
+            //Arrange
+            var validFloatString = "3.1415";
+
+            //Act
+            var kataFloatResult = KataFloat32.Parse(validFloatString);
+
+            //Assert
+            Assert.Equal(kataFloatResult._sign, 0);
+            Assert.Equal(kataFloatResult._exponent, 0b10000000);
+            Assert.Equal(kataFloatResult._mantissa, 0b10010010000111001010110);
+        }
+    }
+}
