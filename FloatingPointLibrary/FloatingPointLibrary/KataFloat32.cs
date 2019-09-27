27cc4ff729c61738ae25ecf02afb0662deb07f95
Francois Du Plessis <Francois.DuPlessis@Euromonitor.com>
2019-09-27 11:17:45 +0200

    Updated the code as we have it so far

--- FloatingPointLibrary/FloatingPointLibrary/KataFloat32.cs
+++ FloatingPointLibrary/FloatingPointLibrary/KataFloat32.cs
@@ -4,16 +4,41 @@ using System.Text;
 
 namespace FloatingPointLibrary
 {
-    public class KataFloat32
+    public struct KataFloat32
     {
-
         internal int _sign;
-        internal  _exponent;
-        internal 
+        internal uint _exponent;
+        internal uint _mantissa;
 
         public KataFloat32 Parse(string number)
         {
-            return 
+            KataFloat32.TryParse("asdasd", out var kataFloat, out _);
         }
-     }
+
+        public static bool TryParse(string number, out KataFloat32 kataFloat, out bool isExact)
+        {
+
+        }
+
+        public static bool TryParse(string number, out KataFloat32 kataFloat)
+        {
+
+        }
+
+        public static KataFloat32 operator* (KataFloat32 x, KataFloat32 y)
+        {
+            return new KataFloat32();
+        }
+
+        // Assume no c# float
+        // Implicity cast between integer and KataFloat32
+        // Tell user if the we could parse their float exactly
+        // Comparison between two floats (how accurate are we going to be)
+
+
+        // Talk about this more
+        // Think about our users
+        //
+
+    }
 }
--- FloatingPointLibrary/FloatingPointLibrary/Program.cs
+++ FloatingPointLibrary/FloatingPointLibrary/Program.cs
@@ -6,7 +6,21 @@ namespace FloatingPointLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            KataFloat32 kata = new KataFloat32();
+            kata._sign = 5;
+
+            int x = 1;
+            XYZ(kata);
+
+
+            Console.WriteLine($"{x}");
+            Console.ReadLine();
+        }
+
+        public static void XYZ(KataFloat32 x)
+        {
+            x = new KataFloat32();
+            x._sign = 4;
         }
     }
 }
